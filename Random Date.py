0bb446758df537800e0e644b9562d1b91cf40daf
Rishal-Jr <rmrazni@gmail.com>
2025-02-10 19:22:20 +0530

    Randdate

---
+++ Random Date.py
@@ -0,0 +1,12 @@
+import random
+import time
+def getdatetime(stardate,enddate):
+    print("Printing random date between",stardate,"and",enddate)
+    randomgenerator=random.random()
+    dateformate="%m/%d/%y"
+    starttime=time.mktime(time.strptime(stardate,dateformate))
+    endtime=time.mktime(time.strptime(enddate,dateformate))
+    randomtime=starttime+randomgenerator*(endtime-starttime)
+    randomdate=time.strftime(dateformate,time.localtime(randomtime))
+    return randomdate
+print("Random Date=",getdatetime(1/1/2020,12/12/2024))
